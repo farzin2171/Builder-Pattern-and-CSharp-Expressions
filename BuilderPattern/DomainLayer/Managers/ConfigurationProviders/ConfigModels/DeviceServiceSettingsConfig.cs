783eb9624ab2ff4bdae13662c149b765461e3cad
Farzin Faghirnavaz <ffaghirnavaz@safefleet.net>
2020-07-13 14:11:50 -0400

    Working prograss

---
+++ BuilderPattern/DomainLayer/Managers/ConfigurationProviders/ConfigModels/DeviceServiceSettingsConfig.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DomainLayer.Managers.ConfigurationProviders.ConfigModels
+{
+    internal sealed class DeviceServiceSettingsConfig
+    {
+        public string BaseUrl { get; set; }
+        public string AccessToken { get; set; }
+        public string HttpProxyUrl { get; set; }
+        public IEnumerable<DeviceConfig> DeviceConfigs { get; set; }
+    }
+}
---
+++ BuilderPattern/DomainLayer/Managers/ConfigurationProviders/ConfigurationProvider.cs
@@ -0,0 +1,8 @@
+﻿
+
+namespace DomainLayer.Managers.ConfigurationProviders
+{
+    class ConfigurationProvider
+    {
+    }
+}
