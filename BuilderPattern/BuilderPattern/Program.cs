6ae403b56bfecc852c89de1119cf5d22f3c70221
Farzin Faghirnavaz <ffaghirnavaz@safefleet.net>
2020-07-13 15:46:22 -0400

    Working prograss

--- BuilderPattern/BuilderPattern/Program.cs
+++ BuilderPattern/BuilderPattern/Program.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using DomainLayer.Managers.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
 
 namespace BuilderPattern
 {
@@ -6,7 +9,53 @@ namespace BuilderPattern
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var deviceServiceSettings = new DeviceServiceSettingsBuilder()
+                .Set(x => x.BaseUrl, "webtech.com")
+                .Build();
+
+            Console.WriteLine(deviceServiceSettings.ToString());
+            Console.ReadLine();
+        }
+
+
+    }
+
+    internal sealed class DeviceServiceSettingsBuilder
+    {
+        private readonly Dictionary<string, object> _propertiesToBuild = new Dictionary<string, object>();
+
+        public DeviceServiceSettingsBuilder Set<T>(Expression<Func<DeviceServiceSettings, T>> expression, T value)
+        {
+            var propertyName = ((MemberExpression)expression.Body).Member.Name;
+            _propertiesToBuild[propertyName] = value;
+            return this;
+        }
+        public DeviceServiceSettings Build()
+        {
+            return new DeviceServiceSettings(
+                baseUrl: _propertiesToBuild.TryGetValue(nameof(DeviceServiceSettings.BaseUrl), out var baseUrl) ? (string)baseUrl : RandomDataGenerator.BaseUrl(),
+                accessToken: RandomDataGenerator.AccessToken(),
+                httpProxyUrl: RandomDataGenerator.Url("proxy"),
+                devices: GenerateDevices());
+        }
+        private IEnumerable<Device> GenerateDevices(int count = 0)
+        {
+            if (count == 0)
+            {
+                count = RandomDataGenerator.Integer(3, 5);
+            }
+
+            var devices = new List<Device>();
+
+            for (int i = 0; i < count; i++)
+            {
+                devices.Add(new Device(
+                    identifier: RandomDataGenerator.AlphaString(12),
+                    type: RandomDataGenerator.AlphaString(8),
+                    otaKey: RandomDataGenerator.AccessToken()));
+            }
+
+            return devices;
         }
     }
 }
